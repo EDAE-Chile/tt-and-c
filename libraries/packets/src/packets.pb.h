a46ae73f211f92cb2cdf6b2aff00508f535161f9
Agustín Covarrubias <agucova@uc.cl>
2021-02-08 15:47:45 -0300

    feat!: remove ignition and change coords to float

--- libraries/packets/src/packets.pb.h
+++ libraries/packets/src/packets.pb.h
@@ -32,14 +32,13 @@ typedef struct _TelemetryPacket_IMU {
 } TelemetryPacket_IMU;
 
 typedef struct _TelemetryPacket {
-    bool ignition;
     bool parachute_open;
     bool payload_separated;
     TelemetryPacket_GPSStatus gps_reporting_status;
     bool has_latitude;
-    double latitude;
+    float latitude;
     bool has_longitude;
-    double longitude;
+    float longitude;
     bool mobileStatus;
     bool has_signal_strength;
     float signal_strength;
@@ -66,9 +65,9 @@ extern "C" {
 #endif
 
 /* Initializer values for message structs */
-#define TelemetryPacket_init_default             {0, 0, 0, _TelemetryPacket_GPSStatus_MIN, false, 0, false, 0, 0, false, 0, 0, 0, 0, false, TelemetryPacket_IMU_init_default, false, 0, false, 0}
+#define TelemetryPacket_init_default             {0, 0, _TelemetryPacket_GPSStatus_MIN, false, 0, false, 0, 0, false, 0, 0, 0, 0, false, TelemetryPacket_IMU_init_default, false, 0, false, 0}
 #define TelemetryPacket_IMU_init_default         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-#define TelemetryPacket_init_zero                {0, 0, 0, _TelemetryPacket_GPSStatus_MIN, false, 0, false, 0, 0, false, 0, 0, 0, 0, false, TelemetryPacket_IMU_init_zero, false, 0, false, 0}
+#define TelemetryPacket_init_zero                {0, 0, _TelemetryPacket_GPSStatus_MIN, false, 0, false, 0, 0, false, 0, 0, 0, 0, false, TelemetryPacket_IMU_init_zero, false, 0, false, 0}
 #define TelemetryPacket_IMU_init_zero            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 
 /* Field tags (for use in manual encoding/decoding) */
@@ -82,7 +81,6 @@ extern "C" {
 #define TelemetryPacket_IMU_magnetometer_y_tag   8
 #define TelemetryPacket_IMU_magnetometer_z_tag   9
 #define TelemetryPacket_IMU_secondary_temperature_tag 10
-#define TelemetryPacket_ignition_tag             1
 #define TelemetryPacket_parachute_open_tag       2
 #define TelemetryPacket_payload_separated_tag    3
 #define TelemetryPacket_gps_reporting_status_tag 4
@@ -99,12 +97,11 @@ extern "C" {
 
 /* Struct field encoding specification for nanopb */
 #define TelemetryPacket_FIELDLIST(X, a) \
-X(a, STATIC,   SINGULAR, BOOL,     ignition,          1) \
 X(a, STATIC,   SINGULAR, BOOL,     parachute_open,    2) \
 X(a, STATIC,   SINGULAR, BOOL,     payload_separated,   3) \
 X(a, STATIC,   SINGULAR, UENUM,    gps_reporting_status,   4) \
-X(a, STATIC,   OPTIONAL, DOUBLE,   latitude,          5) \
-X(a, STATIC,   OPTIONAL, DOUBLE,   longitude,         6) \
+X(a, STATIC,   OPTIONAL, FLOAT,    latitude,          5) \
+X(a, STATIC,   OPTIONAL, FLOAT,    longitude,         6) \
 X(a, STATIC,   SINGULAR, BOOL,     mobileStatus,      7) \
 X(a, STATIC,   OPTIONAL, FLOAT,    signal_strength,   8) \
 X(a, STATIC,   SINGULAR, BOOL,     imu_Status,        9) \
@@ -139,7 +136,7 @@ extern const pb_msgdesc_t TelemetryPacket_IMU_msg;
 #define TelemetryPacket_IMU_fields &TelemetryPacket_IMU_msg
 
 /* Maximum encoded size of messages (where known) */
-#define TelemetryPacket_size                     101
+#define TelemetryPacket_size                     91
 #define TelemetryPacket_IMU_size                 50
 
 #ifdef __cplusplus
